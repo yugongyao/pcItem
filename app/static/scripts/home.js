8c94d7565be8a4e60fb7f0d7a3432aabf61dfa79
YuGYAO <ygy851934017@qq.com>
2019-10-04 18:42:38 +0800

    信息

--- app/static/scripts/home.js
+++ app/static/scripts/home.js
@@ -1,1 +1,20 @@
-// var $ = require('jquery');
+$(function () {
+  var $tree = $('.side-tree');
+  var $menu = $('.tree-menu');
+  var $arrow = $('.arrow');
+  var $blue=$('.b-style');
+  var parIndex=0;
+  // 侧栏点击出现消失
+  $.each($tree,function (index,value) { 
+    $tree.eq(index).click(function (e) {
+      $menu.eq(index).toggle(500);
+      $arrow.eq(index).toggleClass('arrow2');
+      $blue.eq(parIndex).removeClass('.line');
+      $blue.eq(index).addClass('.line');
+      parIndex=$(this).index();
+    })
+    
+  });
+
+
+})
